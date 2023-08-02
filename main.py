fc7cb45f29427d1db1707b6595dd682037edf7aa
Cassiano De Souza <cassianocdesouza@gmail.com>
2023-08-02 12:10:05 +0100

    Ajustada a estrutura para melhor alinhamento

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pytest
 
 
 ############################################################
+
+
 def imprimir_oi(nome):
     print(f'Oi, {nome}')
 
@@ -15,6 +17,7 @@ def somar(numero_a, numero_b):
 
 ############################################################
 
+
 # Subtrair
 def subtrair(numero_a, numero_b):
     return numero_a - numero_b
@@ -22,12 +25,14 @@ def subtrair(numero_a, numero_b):
 
 ############################################################
 
+
 # Multiplicar
 def multiplicar(numero_a, numero_b):
     return numero_a * numero_b
 
 
 ############################################################
+
 
 # Dividir
 def dividir(numero_a, numero_b):
@@ -38,11 +43,14 @@ def dividir(numero_a, numero_b):
 
 
 ############################################################
+
+
 if __name__ == '__main__':  # Esse é um IF especial de Python, uma varinha magica, e por isso não muda
     imprimir_oi('Cassiano')
 
     resultado = somar(5, 7)
     print(f'A soma: {resultado}')
+
 
 ############################################################
 # Teste Git
